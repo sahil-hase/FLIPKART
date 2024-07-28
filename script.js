88f4a27e504ad86e00755c635b11245dafa66147
sahil-hase <162993831+sahil-hase@users.noreply.github.com>
2024-07-28 14:02:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var swiper1 = new Swiper(".mySwiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var sw1 = new Swiper(".mysw1", {
+  slidesPerView: 4.6,
+  spaceBetween: 30,
+  
+//  autoplay:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+}); 
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 7,
+  spaceBetween: 20 ,
+  
+//  autoplay:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+}); 
